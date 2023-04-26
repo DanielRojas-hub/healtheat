b8f6aaafc47c652142b4c0836b2315fa2fe5f9ac
DanielRojas-hub <69920982+DanielRojas-hub@users.noreply.github.com>
2023-04-26 10:40:28 -0400

    common and client folder

---
+++ lib/client/favorites/view/favorites_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../favorites.dart';
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const FavoritesView();
+  }
+}
