a830063e5826dd68f1c6a294e080af42ae91c408
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-04-30 11:44:50 -0400

    Create files

--- lib/client/favorites/view/favorites_view.dart
+++ lib/client/favorites/view/favorites_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:healtheat/common/widgets/card_food.dart';
 
 class FavoritesView extends StatelessWidget {
   const FavoritesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: CardFood(),
+    );
   }
 }
