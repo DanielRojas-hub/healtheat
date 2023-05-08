c88e497f9de5e5bfe24275085632cfab76e18241
abrilsar <a.sar@correo.unimet.edu.ve>
2023-05-08 11:25:22 -0400

    "SearchBar"

--- lib/client/search/view/search_view.dart
+++ lib/client/search/view/search_view.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:healtheat/common/widgets/custom_card_restaurant.dart';
+import 'package:healtheat/common/widgets/custom_label_buttom.dart';
+import 'package:healtheat/common/widgets/custom_search_bar.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //     appBar: AppBar(),
+    //     body: ListView(
+    //         padding: const EdgeInsets.symmetric(horizontal: 20),
+    //         children: [
+    //           ListView.separated(
+    //             shrinkWrap: true,
+    //             physics: const ScrollPhysics(),
+    //             itemBuilder: (context, index) {
+    //               return CustomCardRestaurant(
+    //                 name: "Hoshi-Sushi",
+    //                 url:
+    //                     'https://hips.hearstapps.com/hmg-prod/images/dsc01939-1638289406.jpg',
+    //                 price: '\$15 - \$20',
+    //                 time: '30-40 min',
+    //                 rate: '4.5',
+    //                 typeFood: const ['Sushi', 'European'],
+    //                 onTap: () {},
+    //                 isFavorite: true,
+    //               );
+    //             },
+    //             itemCount: 10,
+    //             separatorBuilder: (BuildContext context, int index) {
+    //               return const SizedBox(height: 20);
+    //             },
+    //           )
+    //         ]));
     return Scaffold(
         appBar: AppBar(),
-        body: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            children: [
-              ListView.separated(
-                shrinkWrap: true,
-                physics: const ScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return CustomCardRestaurant(
-                    name: "Hoshi-Sushi",
-                    url:
-                        'https://hips.hearstapps.com/hmg-prod/images/dsc01939-1638289406.jpg',
-                    price: '\$15 - \$20',
-                    time: '30-40 min',
-                    rate: '4.5',
-                    typeFood: const ['Sushi', 'European'],
-                    onTap: () {},
-                    isFavorite: false,
-                  );
-                },
-                itemCount: 10,
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(height: 20);
-                },
-              )
-            ]));
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(),
+        ));
   }
 }
