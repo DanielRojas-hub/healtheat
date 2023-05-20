f6d3dd4caf0a1314ba80e6df423d5c95d006d547
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2023-05-20 17:42:51 -0400

    Favorites view

--- lib/client/favorites/view/favorites_view.dart
+++ lib/client/favorites/view/favorites_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:healtheat/common/controllers/tab/tab_cubit.dart';
 import 'package:healtheat/common/utils/constants.dart';
 import 'package:healtheat/common/widgets/card_food.dart';
+import 'package:healtheat/common/widgets/custom_card_restaurant.dart';
+import 'package:healtheat/common/widgets/search_section.dart';
 import 'package:healtheat/common/widgets/tab_widget.dart';
 
 import '../favorites.dart';
@@ -17,6 +19,12 @@ class FavoritesView extends StatelessWidget {
       child: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
+            const SliverToBoxAdapter(child: SizedBox(height: 25)),
+            const SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: Constants.margin),
+              sliver: SliverToBoxAdapter(child: SearchSection()),
+            ),
+            const SliverToBoxAdapter(child: SizedBox(height: 25)),
             SliverPadding(
               padding: const EdgeInsets.symmetric(horizontal: Constants.margin),
               sliver: SliverToBoxAdapter(
@@ -36,29 +44,21 @@ class FavoritesView extends StatelessWidget {
                 context.read<TabCubit>().onPageChanged(index),
             controller: context.read<TabCubit>().controller,
             children: [
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const ScrollPhysics(),
+              ListView.separated(
                 padding:
                     const EdgeInsets.symmetric(horizontal: Constants.margin),
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 0.85,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
-                itemBuilder: (BuildContext context, int index) {
-                  return CardFood(
-                    url:
-                        'https://9a3dd0f30ec2813b0355-0aefa298f29a1e32e1203150d51d4825.ssl.cf1.rackcdn.com/pix_5_101918_80061_6293d1fea56d1.jpg',
-                    title: const Text('Pizza de cartón'),
-                    subtitle: const Text('Gluten free'),
-                    price: const Text('\$99,9'),
-                    isFavorite: false,
-                    onTap: () {},
-                    onTapFavorite: () {},
-                    onTapPrice: () {},
-                  );
-                },
+                itemBuilder: (BuildContext context, int index) =>
+                    const CustomCardRestaurant(
+                        name: "Vero Vero",
+                        url:
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Restaurant_N%C3%A4sinneula.jpg/1200px-Restaurant_N%C3%A4sinneula.jpg",
+                        price: "\$10 - \$15",
+                        rate: "4,8",
+                        time: "25-35 min",
+                        typeFood: ['Pizza', 'Italian'],
+                        isFavorite: false),
+                separatorBuilder: (BuildContext context, int index) =>
+                    const SizedBox(height: 15),
                 itemCount: 5,
               ),
               GridView.builder(
@@ -91,5 +91,3 @@ class FavoritesView extends StatelessWidget {
     );
   }
 }
-
-/*  */
