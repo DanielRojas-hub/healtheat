b33447cdcc6392990d6f804ff94e170e54b5e465
JD-Flores <90649355+JD-Flores@users.noreply.github.com>
2023-05-21 18:43:51 -0400

    Order detail design

--- lib/client/order_detail/view/order_detail_view.dart
+++ lib/client/order_detail/view/order_detail_view.dart
@@ -20,47 +20,176 @@ class OrderDetailView extends StatelessWidget {
             color: Colors.grey[50],
             child: ListView(
               children: [
-                const CustomBackgroundWidget(child: SizedBox(height: 15)),
-                CustomBackgroundWidget(
-                  child: Text('Ordering From',
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
                 const CustomBackgroundWidget(child: CustomDivider()),
-                CustomBackgroundWidget(
-                  child: Text('Delivery To:',
-                      style: Theme.of(context).textTheme.titleMedium),
-                ),
                 CustomBackgroundWidget(
                   child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Address',
-                            style: Theme.of(context).textTheme.labelSmall),
-                        Text(
-                          'New address',
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelMedium
-                              ?.copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                  decoration: TextDecoration.underline),
-                        ),
-                      ]),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text('Order ID',
+                              style: Theme.of(context).textTheme.titleMedium),
+                          Text('#000000',
+                              style: Theme.of(context).textTheme.labelSmall),
+                        ],
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.copy, color: Colors.grey)),
+                    ]
+                        .map((widget) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: widget,
+                            ))
+                        .toList(),
+                  ),
                 ),
                 const CustomBackgroundWidget(child: CustomDivider()),
                 CustomBackgroundWidget(
-                  child: Text('Items in Cart',
-                      style: Theme.of(context).textTheme.titleMedium),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .tertiaryContainer,
+                              child: const Icon(Icons.access_time,
+                                  color: Colors.grey),
+                            ),
+                            const SizedBox(height: 30),
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .tertiaryContainer,
+                              child: const Icon(Icons.soup_kitchen,
+                                  color: Colors.grey),
+                            ),
+                            const SizedBox(height: 30),
+                            CircleAvatar(
+                              radius: 25,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .tertiaryContainer,
+                              child: const Icon(Icons.delivery_dining,
+                                  color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('In Process',
+                                  style:
+                                      Theme.of(context).textTheme.titleSmall),
+                              Text('Process of your order',
+                                  style:
+                                      Theme.of(context).textTheme.labelSmall),
+                            ],
+                          ),
+                          const SizedBox(height: 45),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Cooking',
+                                  style:
+                                      Theme.of(context).textTheme.titleSmall),
+                              Text('Your order is getting ready',
+                                  style:
+                                      Theme.of(context).textTheme.labelSmall),
+                            ],
+                          ),
+                          const SizedBox(height: 45),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Delivery',
+                                  style:
+                                      Theme.of(context).textTheme.titleSmall),
+                              Text('Your order is on its way',
+                                  style:
+                                      Theme.of(context).textTheme.labelSmall),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Column(
+                          children: [
+                            Text('12:30',
+                                style: Theme.of(context).textTheme.titleSmall),
+                            const SizedBox(height: 60),
+                            Text('1:30',
+                                style: Theme.of(context).textTheme.titleSmall),
+                            const SizedBox(height: 60),
+                            Text('2:30',
+                                style: Theme.of(context).textTheme.titleSmall),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                const CustomBackgroundWidget(
-                    child:
-                        CustomDivider(margin: EdgeInsets.only(bottom: 15.0))),
-                const CustomBackgroundWidget(child: DottedDivider()),
-                const TotalCard(label: Text('\$28.48')),
-                const CustomBackgroundWidget(
-                    child: DottedDivider(padding: EdgeInsets.only(top: 15))),
-                const SizedBox(height: 100),
+                const CustomBackgroundWidget(child: CustomDivider()),
+                CustomBackgroundWidget(
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset('assets/images/pfp.jpg', height: 70),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Designated Driver',
+                              style: Theme.of(context).textTheme.titleMedium),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 15,
+                                ),
+                                Text('4.5',
+                                    style:
+                                        Theme.of(context).textTheme.labelSmall),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: CircleAvatar(
+                          radius: 25,
+                          backgroundColor: Theme.of(context).primaryColor,
+                          child: IconButton(
+                              icon: Icon(
+                                Icons.phone_in_talk_rounded,
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                              onPressed: () {}),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
